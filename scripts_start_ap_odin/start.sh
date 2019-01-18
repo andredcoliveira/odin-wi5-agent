b77919484493a8588fcf27bd7035a43d61af09eb
josemariasaldana <jsaldana@unizar.es>
2019-01-18 12:49:27 +0100

    Improved start.sh
    I have included some improvements:
    - a number of variables for storing the names of the interfaces.
    - stop the network manager to avoid problems.

--- scripts_start_ap_odin/start.sh
+++ scripts_start_ap_odin/start.sh
@@ -15,23 +15,72 @@
 
 ## Variables
 echo "Setting variables"
-CTLIP=192.168.1.129 # Controller IP address
-SW=br0              # Name of the bridge
-DPPORTS="eth1.2"    # Port for data plane
-VSCTL="ovs-vsctl"   # Command to be used to invoke openvswitch
+CTLIP=192.168.1.129         # Controller IP address
+SW=br0                      # Name of the bridge
+CPINTERFACE="eth1"          # Interface for the control plane
+#DPINTERFACE="eth1.2"       # Interface for the data plane (eth1.2 works in the TP-Link AP)
+DPINTERFACE="eth2"          # Interface for the data plane (eth2 works in a PC)
+TAPINTERFACE="ap"           # tap port for connecting to the Internet
+MON0INTERFACE="mon0"        # main wireless interface in monitor mode
+MON1INTERFACE="mon1"        # auxiliary wireless interface in monitor mode
+WIRELESS0INTERFACE="wlan0"  # main wireless interface
+WIRELESS1INTERFACE="wlan1"  # auxiliary wireless interface
+PHY0INTERFACE="phy0"        # main wireless physical interface
+PHY1INTERFACE="phy1"        # auxiliary wireless physical interface
+VSCTL="ovs-vsctl"           # Command to be used to invoke openvswitch
+
+## Stopping Network Manager
+# If Network Manager is running, it may modify the file /sys/kernel/debug/ieee80211/phy0/ath9k/bssid_extra
+#of this machine, setting it to ff:ff:ff:ff:ff:ff. So the best option is to switch it off
+echo "Stopping Network Manager"
+service NetworkManager stop
 
 ## Setting interfaces
 echo "Setting interfaces"
-ifconfig wlan0 down
-ifconfig wlan1 down
-iw phy phy0 interface add mon0 type monitor
-iw phy phy1 interface add mon1 type monitor
-ifconfig mon0 up
-ifconfig mon1 up
-ifconfig mon0 mtu 1532
-ifconfig mon1 mtu 1532
-ifconfig wlan0 up
-ifconfig wlan1 up
+ifconfig $CPINTERFACE 192.168.1.1
+ifconfig $DPINTERFACE 192.168.2.1
+sleep 0.5
+ifconfig $WIRELESS0INTERFACE down
+sleep 0.5
+ifconfig $WIRELESS1INTERFACE down
+sleep 0.5
+iw phy $PHY0INTERFACE interface add $MON0INTERFACE type monitor
+echo "Added $MON0INTERFACE interface"
+sleep 0.5
+iw phy $PHY1INTERFACE interface add $MON1INTERFACE type monitor
+echo "Added $MON1INTERFACE interface"
+sleep 0.5
+ifconfig $MON0INTERFACE down
+sleep 0.5
+ifconfig $MON1INTERFACE down
+sleep 0.5
+iwconfig $MON0INTERFACE mode monitor
+sleep 0.5
+iwconfig $MON1INTERFACE mode monitor
+sleep 0.5
+ifconfig $MON0INTERFACE up
+echo "$MON0INTERFACE is now up"
+sleep 0.5
+ifconfig $MON1INTERFACE up
+echo "$MON1INTERFACE is now up"
+sleep 0.5
+ifconfig $MON0INTERFACE mtu 1532
+sleep 0.5
+ifconfig $MON1INTERFACE mtu 1532
+sleep 0.5
+iw $PHY0INTERFACE set channel 9
+echo "$PHY0INTERFACE is now in channel 9"
+sleep 0.5
+iw $PHY1INTERFACE set channel 9
+echo "$PHY1INTERFACE is now in channel 9"
+sleep 0.5
+ifconfig $WIRELESS0INTERFACE up
+echo "$WIRELESS0INTERFACE is now up"
+sleep 0.5
+ifconfig $WIRELESS1INTERFACE up
+echo "$WIRELESS1INTERFACE is now up"
+sleep 0.5
+
 
 ## Routes
 # add these routes in order to permit control from other networks (this is very particular of Unizar)
@@ -39,41 +88,32 @@ ifconfig wlan1 up
 route add -net 155.210.158.0 netmask 255.255.255.0 gw 155.210.157.254 eth0
 route add -net 155.210.156.0 netmask 255.255.255.0 gw 155.210.157.254 eth0
 
-# set the default gateway where masquerading is being performed
-#route del default gw 155.210.157.254
-#route add default gw 192.168.1.131
 
 ## OVS
 echo "Restarting OpenvSwitch"
-/etc/init.d/openvswitch stop
+/etc/init.d/openvswitch-switch stop
 sleep 1
+#rmmod openvswitch-switch
 # The next line is added in order to start the controller after stopping openvswitch
 read -p "Now you can launch the Wi-5 odin controller and press Enter" pause
+
 
 # Clean the OpenVSwitch database
 if [ -d "/etc/openvswitch" ]; then
   echo "OpenVSwitch folder already exists"
+  echo "Cleaning OpenVSwitch database"
+  rm /etc/openvswitch/*
 else
   echo "OpenVSwitch folder created"
   mkdir /etc/openvswitch
 fi
-if [ -f "/etc/openvswitch/conf.db" ]; then
-  echo "Cleaning OpenVSwitch database"
-  rm /etc/openvswitch/conf.db
-fi
-if [ -f "/var/run/db.sock" ]; then
-  rm /var/run/db.sock
-fi
-if [ -f "/var/run/ovsdb-server.pid" ]; then
-  rm /var/run/ovsdb-server.pid
-fi
-if [ -f "/var/run/ovs-vswitchd.pid" ]; then
-  rm /var/run/ovs-vswitchd.pid
+if [ -d "/var/run/openvswitch" ]; then
+  rm /var/run/openvswitch/*
 fi
 
 # Launch OpenVSwitch
 echo "Launching OpenVSwitch"
-/etc/init.d/openvswitch start
+/etc/init.d/openvswitch-switch start
 
 # Create the bridge
 $VSCTL add-br $SW
@@ -83,7 +123,7 @@ ifconfig $SW up # In OpenWrt 15.05 the bridge is created down
 $VSCTL set-controller $SW tcp:$CTLIP:6633
 
 # Add the data plane ports to OpenVSwitch
-for i in $DPPORTS ; do
+for i in $DPINTERFACE ; do
   PORT=$i
   ifconfig $PORT up
   $VSCTL add-port $SW $PORT
@@ -94,29 +134,31 @@ sleep 3
 echo "Launching Click"
 
 # Mount USB if you need it for putting the Click ('click') and Click-align ('click-al') binaries
-echo "Mounting USB"
-if [ ! -d "/mnt/usb" ]; then
-  mkdir -p /mnt/usb
-fi
-mount /dev/sda1 /mnt/usb/
+#echo "Mounting USB"
+#if [ ! -d "/mnt/usb" ]; then
+#  mkdir -p /mnt/usb
+#fi
+#mount /dev/sda1 /mnt/usb/
 
-cd /mnt/usb
-./click < click-al agent.cli &    # This makes the alignment and calls Click at the same time
+
+#cd /mnt/usb
+#./click < click-al agent.cli &    # This makes the alignment and calls Click at the same time
+./click/bin/click agentpc.cli &    # This calls Click
 #./click aagent.cli &             # Old command, which required an aligned version of the .cli file
 sleep 1
-# From this moment, a new tap interface called 'ap' will be created by Click
+# From this moment, a new tap interface called 'TAPINTERFACE' will be created by Click
 
-# Add the 'ap' interface to OpenVSwitch
-echo "Adding Click interface 'ap' to OVS"
-ifconfig ap up            # Putting the interface 'ap' up
-$VSCTL add-port $SW ap    # Adding 'ap' interface (click Interface) to OVS
+# Add the 'TAPINTERFACE' interface to OpenVSwitch
+echo "Adding Click interface '$TAPINTERFACE' to OVS"
+ifconfig $TAPINTERFACE up            # Putting the interface '$TAPINTERFACE' up
+$VSCTL add-port $SW $TAPINTERFACE    # Adding 'TAPINTERFACE' interface (click Interface) to OVS
 sleep 1
 
 ## OpenVSwitch Rules
 # OpenFlow rules needed to make it possible for DHCP traffic to arrive to the Wi-5 odin controller
-# It may happen that 'eth1.2' is port 1 and 'ap' is port 2
+# It may happen that the data plane port is port 1 and the tap port is port 2
 ovs-ofctl add-flow br0 in_port=2,dl_type=0x0800,nw_proto=17,tp_dst=67,actions=output:1,CONTROLLER
 ovs-ofctl add-flow br0 in_port=1,dl_type=0x0800,nw_proto=17,tp_dst=68,actions=output:CONTROLLER,2
-# It may happen that 'eth1.2' is port 2 and 'ap' is port 1
+# It may happen that the data plane port is port 2 and the tap port is port 1
 ovs-ofctl add-flow br0 in_port=1,dl_type=0x0800,nw_proto=17,tp_dst=67,actions=output:2,CONTROLLER
 ovs-ofctl add-flow br0 in_port=2,dl_type=0x0800,nw_proto=17,tp_dst=68,actions=output:CONTROLLER,1
